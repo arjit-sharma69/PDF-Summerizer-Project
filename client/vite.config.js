9c93727dc5a10746f0823aabf65f747e893b6dd5
Arjit-Sharma <2k22.csai.2211823@gmail.com>
2025-08-30 23:06:31 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const target = env.VITE_API_BASE || 'http://localhost:5000';
+    const target = 'https://pdf-summerizer-project-backend.onrender.com'
   return defineConfig({
     plugins: [react()],
     server: {
